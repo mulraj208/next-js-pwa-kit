972c4f05919d3433528a4ea88e6cb025354d03df
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-11-06 16:11:59 +0530

    Refactor `fetch-product-data`

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,47 +1,10 @@
 import React from 'react';
 import ProductView from "@/app/product/[productId]/product-view";
-import Auth from "@salesforce/commerce-sdk-react/auth";
-import {getAuthInstance} from "@/auth";
-import {authConfig} from "@/auth/auth-config";
-import {ShopperProducts} from "commerce-sdk-isomorphic";
-import config from '@/config/dw'
-
-async function fetchProductData(auth: Auth, productId: string) {
-    try {
-        const {access_token} = await auth.ready();
-
-        const shopperProducts = new ShopperProducts({
-            parameters: {
-                clientId: config.CLIENT_ID,
-                organizationId: config.ORGANIZATION_ID,
-                redirectURI: `${process.env.NEXT_PUBLIC_APP_ORIGIN}/callback`,
-                proxy: `${process.env.NEXT_PUBLIC_APP_ORIGIN}/mobify/proxy/api`,
-                siteId: config.SITE_ID,
-                shortCode: config.SHORT_CODE,
-                locale: "en-US",
-                currency: "USD",
-            },
-            proxy: `${process.env.NEXT_PUBLIC_APP_ORIGIN}/mobify/proxy/api`
-        });
-
-        return await shopperProducts.getProduct({
-            parameters: {
-                id: productId,
-                allImages: true
-            },
-            headers: {
-                Authorization: `Bearer ${access_token}`
-            }
-        });
-    } catch (error) {
-        console.error('Failed to fetch product data:', error);
-    }
-}
+import {fetchProductData} from "@/utils/fetch-product-data";
 
 export default async function ProductDetail({params}: { params: { productId: string } }) {
     const {productId} = params;
-    const auth = getAuthInstance(authConfig)
-    const productData = await fetchProductData(auth, productId);
+    const productData = await fetchProductData(productId);
 
     return (
         <ProductView productData={productData} />
